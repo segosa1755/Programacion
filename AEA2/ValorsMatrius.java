6781f9697e3a692aa73d0906404f90c5863fc228
segosa1755 <segosa@pratfp.com>
2025-11-14 00:10:55 +0000

    ValorsMatriusMejorado

--- AEA2/ValorsMatrius.java
+++ AEA2/ValorsMatrius.java
@@ -1,17 +1,36 @@
-public class Exemplesmatrius {
-	public static void main(String[] args){
-		int [] [] matriu ={
-			{1, 2, 3},
-			{4, 5, 6},
-			{7, 8, 9}
-		};
+import java.util.Scanner;
 
-		System.out.println("Elements de la matriu:");
-		for (int i =0; i < matriu.length; i++){
-			for (int j = 0; j < matriu[i].length; j++){
-				System.out.print(matriu[i] [j] + "");
-			}
-			System.out.println();
-		}
-	}
+public class ValorsMatrius {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("¿Cuántas filas tendrá la matriz? ");
+        int files = s.nextInt();
+        System.out.print("¿Cuántas columnas tendrá la matriz? ");
+        int columnes = s.nextInt();
+        int[][] matriu = new int[files][columnes];
+        System.out.println("Introduce los valores de la matriz:");
+        for (int i = 0; i < files; i++) {
+            for (int j = 0; j < columnes; j++) {
+                System.out.print("Valor en [" + i + "][" + j + "]: ");
+                matriu[i][j] = s.nextInt();
+            }
+        }
+        System.out.println("Matriz transformada:");
+        for (int i = 0; i < files; i++) {
+            for (int j = 0; j < columnes; j++) {
+
+                if (i % 2 == 0 && j % 2 == 0) { 
+                    matriu[i][j] = 1;
+                } else if (i % 2 == 0 && j % 2 != 0) {
+                    matriu[i][j] = 2;
+                } else if (i % 2 != 0 && j % 2 == 0) {
+                    matriu[i][j] = 3;
+                } else {
+                    matriu[i][j] = 4;
+                }
+                System.out.print(matriu[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
