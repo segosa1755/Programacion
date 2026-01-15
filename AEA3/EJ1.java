24e3a280ec2dfce763a7712b6e378a910bb2efd9
segosa1755 <segosa@pratfp.com>
2026-01-15 10:13:36 +0000

    Primer ej de aea3

---
+++ AEA3/EJ1.java
@@ -0,0 +1,19 @@
+public class EJ1 {
+
+    public static void main(String[] args) {
+
+        imprimir('*', 5);
+        imprimir('#', 3);
+        imprimir('A', 4);
+
+    }
+
+    public static void imprimir(char caracter, int veces) {
+
+        for (int i = 0; i < veces; i++) {
+            System.out.print(caracter);
+        }
+        System.out.println();
+
+    }
+}
